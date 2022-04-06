de5aa2b7fc933971d7726fab150acc3f73701de8
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-04-06 15:47:29 +0200

    something is wrong so added some css for fun

--- public/logic.js
+++ public/logic.js
@@ -6,6 +6,10 @@ document
   });
 
 async function getMatchas() {
-  let response = await fetch("http://localhost:3000/api/matcha");
-  return await response.json();
+  try {
+    let response = await fetch("http://localhost:3000/api/matcha");
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
